2a6eae68f088f0215b20a5995e7023a1c9892092
Robert Gordon <Komeuppance@gmail.com>
2020-04-01 12:07:58 -0700

    Sorting merge conflicts

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,61 +4,76 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+
 import withContext, { Provider } from '../../Context';
 import styled from "styled-components";
 
-import Attendance from "../Tiles/Attendance.js";
-import AlertTile from '../Tiles/AlertTile';
-import MissingWork from "../Tiles/MissingWork";
+import TileContainer from './Tiles/TileContainer';
 
 const Main = () => {
   return (
-    <div className='main'>
+    <MainWrap>
       <Router>
         <Switch>
           <Route exact path="/" render={props => <div {...props}>Login Page</div>} />
-          <Route path="/dashboard" render={props => <Tile {...props} />} />
+          <Route path="/dashboard" render={props => <TileContainer {...props} />} />
           <Route render={props => <h1>Page was not found.</h1>} />
         </Switch>
       </Router>
-    </div>
+    </MainWrap>
   );
 };
 
-// const Main = () => {
-//   return (
-//     <div className='main'>
-//       <div className="main-container">
-//         <Tile className='tile' title='Alerts'>
-//           <AlertTileWithContext />
-//         </Tile>
-//         <Tile className="tile" title='Missing Work'>
-//           <MissingWork />
-//         </Tile>
-//         <Tile className="tile" title='Attendance'>
-//           <Attendance />
-//         </Tile>
-//         <div className="tile">Tile</div>
-//         <div className="tile">Tile</div>
-//         <div className="tile">Tile</div>
-//       </div>
-//     </div>
-//   );
-// };
+const MainWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  color: white;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    166deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(148, 187, 233, 1) 100%
+  );
+  overflow-y: auto;
+  
+  .tile-container {
+    margin-top: 20px;
+    max-width: 1368px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  
+    .tile {
+      width: 350px;
+      height: 400px;
 
-const Tile = ({ children, title }) => {
-  return (
-    <div className='tile'>
-      {title ? (
-        <div className='tile__header'>
-          { title }
-        </div>
-      ) : (null)}
-      <div className='tile__body'>
-        {children}
-      </div>
-    </div>
-  )
-}
+      background-color: white;
+
+      margin-bottom: 20px;
+      
+      border-radius: 3px;
+      box-shadow: 2px 2px 10px lightgray;
+      
+      &__header {
+        width: 100%;
+        height: 40px;
+        color: white;
+        font-weight: bold;
+        
+        background-color: #1B212D;
+
+        display: flex;
+        align-items: center;
+
+        padding-left: 15px;
+      }
+    }
+  }
+`;
 
 export default Main;
--- src/components/Main/Tiles/TileContainer.js
+++ src/components/Main/Tiles/TileContainer.js
@@ -1,42 +1,37 @@
 import React from "react";
 
-import Attendance from "../../Attendance/Attendance.js";
-import AlertTile from './AlertTile';
+import Attendance from "../../Tiles/Attendance.js";
+import AlertTile from "../../Tiles/AlertTile.js";
+import MissingWork from "../../Tiles/MissingWork.js";
 
-import withContext, { Provider } from '../../../Context';
+import withContext, { Provider } from "../../../Context";
 
 const AlertTileWithContext = withContext(AlertTile);
 
 export default function TileContainer() {
-    return (
-        <div className="tile-container">
-            <Tile title="Alerts">
-                <AlertTileWithContext />
-            </Tile>
-            <Tile className="tile">
-                List of students who have not submitted most recent retro
-            </Tile>
-            <div className="tile">
-                <Attendance />
-            </div>
-            <div className="tile">Tile</div>
-            <div className="tile">Tile</div>
-            <div className="tile">Tile</div>
-        </div>
-    );
+  return (
+    <div className="tile-container">
+      <Tile title="Alerts">
+        <AlertTileWithContext />
+      </Tile>
+      <Tile className="tile" title="Missing Work">
+        <MissingWork />
+      </Tile>
+      <Tile className="tile" title="Attendance">
+        <Attendance />
+      </Tile>
+      <div className="tile">Tile</div>
+      <div className="tile">Tile</div>
+      <div className="tile">Tile</div>
+    </div>
+  );
 }
 
 const Tile = ({ children, title }) => {
-    return (
-      <div className='tile'>
-        {title ? (
-          <div className='tile__header'>
-            { title }
-          </div>
-        ) : (null)}
-        <div className='tile__body'>
-          {children}
-        </div>
-      </div>
-    )
-  }
+  return (
+    <div className="tile">
+      {title ? <div className="tile__header">{title}</div> : null}
+      <div className="tile__body">{children}</div>
+    </div>
+  );
+};
