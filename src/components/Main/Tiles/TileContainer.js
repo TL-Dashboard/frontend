f3e035e90adbd66dd334eb648baf0f373db2ab60
Robert Gordon <Komeuppance@gmail.com>
2020-04-01 13:42:22 -0700

    Merge conflicts fixed, ready for merge

--- src/components/Main/Tiles/TileContainer.js
+++ src/components/Main/Tiles/TileContainer.js
@@ -1,42 +1,37 @@
 import React from "react";
 
 import Attendance from "../../Tiles/Attendance";
-import AlertTile from '../../Tiles/AlertTile';
+import AlertTile from "../../Tiles/AlertTile";
+import MissingWork from "../../Tiles/MissingWork.js";
 
-import withContext, { Provider } from '../../../Context';
+import withContext, { Provider } from "../../../Context";
 
 const AlertTileWithContext = withContext(AlertTile);
 
 export default function TileContainer() {
-    return (
-        <div className="main-container">
-            <Tile title="Alerts">
-                <AlertTileWithContext />
-            </Tile>
-            <Tile className="tile">
-                List of students who have not submitted most recent retro
-            </Tile>
-            <Tile title="Attendance" className="tile">
-                <Attendance />
-            </Tile>
-            <Tile className="tile">Tile</Tile>
-            <Tile className="tile">Tile</Tile>
-            <Tile className="tile">Tile</Tile>
-        </div>
-    );
+  return (
+    <div className="main-container">
+      <Tile title="Alerts">
+        <AlertTileWithContext />
+      </Tile>
+      <Tile className="tile" title="Missing Work">
+        <MissingWork />
+      </Tile>
+      <Tile className="tile" title="Attendance">
+        <Attendance />
+      </Tile>
+      <Tile className="tile">Tile</Tile>
+      <Tile className="tile">Tile</Tile>
+      <Tile className="tile">Tile</Tile>
+    </div>
+  );
 }
 
 const Tile = ({ children, title }) => {
-    return (
-        <div className='tile'>
-        {title ? (
-            <div className='tile__header'>
-            { title }
-            </div>
-        ) : (null)}
-        <div className='tile__body'>
-            {children}
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="tile">
+      {title ? <div className="tile__header">{title}</div> : null}
+      <div className="tile__body">{children}</div>
+    </div>
+  );
+};
