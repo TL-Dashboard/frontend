84b41ed2e1196d241bdac888227aca4d8bfbc037
Robert Gordon <Komeuppance@gmail.com>
2020-04-01 13:44:05 -0700

    Adjust indentation

--- src/components/Main/Tiles/TileContainer.js
+++ src/components/Main/Tiles/TileContainer.js
@@ -9,29 +9,29 @@ import withContext, { Provider } from "../../../Context";
 const AlertTileWithContext = withContext(AlertTile);
 
 export default function TileContainer() {
-  return (
-    <div className="main-container">
-      <Tile title="Alerts">
-        <AlertTileWithContext />
-      </Tile>
-      <Tile className="tile" title="Missing Work">
-        <MissingWork />
-      </Tile>
-      <Tile className="tile" title="Attendance">
-        <Attendance />
-      </Tile>
-      <Tile className="tile">Tile</Tile>
-      <Tile className="tile">Tile</Tile>
-      <Tile className="tile">Tile</Tile>
-    </div>
-  );
+    return (
+        <div className="main-container">
+            <Tile title="Alerts">
+                <AlertTileWithContext />
+            </Tile>
+            <Tile className="tile" title="Missing Work">
+                <MissingWork />
+            </Tile>
+            <Tile className="tile" title="Attendance">
+                <Attendance />
+            </Tile>
+            <Tile className="tile">Tile</Tile>
+            <Tile className="tile">Tile</Tile>
+            <Tile className="tile">Tile</Tile>
+        </div>
+    );
 }
 
 const Tile = ({ children, title }) => {
-  return (
-    <div className="tile">
-      {title ? <div className="tile__header">{title}</div> : null}
-      <div className="tile__body">{children}</div>
-    </div>
-  );
+    return (
+        <div className="tile">
+            {title ? <div className="tile__header">{title}</div> : null}
+            <div className="tile__body">{children}</div>
+        </div>
+    );
 };
