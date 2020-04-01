22a7d74904effa561792712dc0c8ba9b238b41ce
Alexander Besse <alexanderjoeb@hotmail.com>
2020-04-01 12:35:13 -0500

    Attendance file re-structured.

--- src/components/Tiles/Attendance.js
+++ src/components/Tiles/Attendance.js
@@ -2,8 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 /* CSS goes here */
-const Wrapper = styled.div` 
-
+const Wrapper = styled.div`
     /*alerts box header*/
     .attendance-text {
         background-color: black;
@@ -15,7 +14,8 @@ const Wrapper = styled.div`
         /* box-shadow: 0 8px 6px -6px gray; */
     }
     /*attendance form border, and it's boxes */
-    .attendance-container, .smaller-containers {
+    .attendance-container,
+    .smaller-containers {
         text-align: center;
         align-items: center;
         margin: 5px;
@@ -25,23 +25,29 @@ const Wrapper = styled.div`
     .smaller-containers {
         border: 1px solid lightgray;
         box-shadow: 0 8px 6px -6px gray;
-        background-color: #FCFCFC;
-    } 
+        background-color: #fcfcfc;
+    }
     /*font styling*/
     .attendanceForm {
-        color: #7B7B7B;
+        color: #7b7b7b;
     }
     /*curving edges for form selects, input, button */
-    #dateSelect, #moduleSelect, #studentSelect, #notesInput, #submit-btn {
+    #dateSelect,
+    #moduleSelect,
+    #studentSelect,
+    #notesInput,
+    #submit-btn {
         border-radius: 5px;
-        background-color: #F0F8FF;
+        background-color: #f0f8ff;
     }
-    #startInput, #standInput, #presentInput {
+    #startInput,
+    #standInput,
+    #presentInput {
         cursor: pointer;
     }
     /*button styling */
     #submit-btn {
-        background-color: #00BFFF;
+        background-color: #00bfff;
         width: 60%;
         margin-top: 3px;
         padding-top: 3px;
@@ -52,68 +58,89 @@ const Wrapper = styled.div`
     }
     /*button hover styling */
     #submit-btn:hover {
-        background-color: #80D3FF;
+        background-color: #80d3ff;
         width: 60%;
-        color: #7B7B7B;
+        color: #7b7b7b;
     }
 `;
 
 const Attendance = () => {
-/*other functions */
+    /*other functions */
 
     /* HTML goes here */
-    return(
+    return (
         <Wrapper>
-        <form class="attendanceForm">
-            <div class="attendance-container">
-                <div class="smaller-containers">
-                    <label for="dateSelect">Date </label>
+            <form class="attendanceForm">
+                <div class="attendance-container">
+                    <div class="smaller-containers">
+                        <label for="dateSelect">Date </label>
                         <select id="dateSelect" name="date">
-                            <option value='1'>Today</option>
-                            <option value='2'>Tomorrow</option>
-                            <option value='3'>Yesterday</option>
+                            <option value="1">Today</option>
+                            <option value="2">Tomorrow</option>
+                            <option value="3">Yesterday</option>
                         </select>
-                </div>
-                <div class="smaller-containers">
-                    <label for="moduleSelect">Module </label>
+                    </div>
+                    <div class="smaller-containers">
+                        <label for="moduleSelect">Module </label>
                         <select id="moduleSelect" name="module">
-                            <option value='1'>WEB30</option>
-                            <option value='2'>WEB30</option>
-                            <option value='3'>WEB30</option>
+                            <option value="1">WEB30</option>
+                            <option value="2">WEB30</option>
+                            <option value="3">WEB30</option>
                         </select>
-                </div>
-                <div class="smaller-containers">
-                    <label for="startInput">Start of Class</label>
-                        <input type='checkbox' id="startInput" name="startClass" defaultChecked />
-                    <br/>
-                    <label for="standInput">Stand Up</label>
-                        <input type='checkbox' id="standInput" name="standUp" defaultChecked />
-                    
-                </div>
-                <div class="smaller-containers">
-                    <label for="studentSelect">Student </label>
+                    </div>
+                    <div class="smaller-containers">
+                        <label for="startInput">Start of Class</label>
+                        <input
+                            type="checkbox"
+                            id="startInput"
+                            name="startClass"
+                            defaultChecked
+                        />
+                        <br />
+                        <label for="standInput">Stand Up</label>
+                        <input
+                            type="checkbox"
+                            id="standInput"
+                            name="standUp"
+                            defaultChecked
+                        />
+                    </div>
+                    <div class="smaller-containers">
+                        <label for="studentSelect">Student </label>
                         <select id="studentSelect" name="student">
-                            <option value='student1'>Jane Doe</option>
-                            <option value='student2'>John Smith</option>
-                            <option value='student3'>Eleanor Fant</option>
-                            <option value='student4'>Eric Widget</option>
-                            <option value='student5'>Fig Nelson</option>
-                            <option value='student6'>Hans Down</option>
-                            <option value='student7'>Theodore Handle</option>
-                            <option value='student8'>Guy Mann</option>
-                        </select><br/>
-                    <label for="presentInput">Present</label>
-                        <input type='checkbox' id="presentInput" name="present" defaultChecked />
-                    <label>Notes </label>
-                        <input type='text' id="notesInput" name="notes" maxLength="35" size="5" />
+                            <option value="student1">Jane Doe</option>
+                            <option value="student2">John Smith</option>
+                            <option value="student3">Eleanor Fant</option>
+                            <option value="student4">Eric Widget</option>
+                            <option value="student5">Fig Nelson</option>
+                            <option value="student6">Hans Down</option>
+                            <option value="student7">Theodore Handle</option>
+                            <option value="student8">Guy Mann</option>
+                        </select>
+                        <br />
+                        <label for="presentInput">Present</label>
+                        <input
+                            type="checkbox"
+                            id="presentInput"
+                            name="present"
+                            defaultChecked
+                        />
+                        <label>Notes </label>
+                        <input
+                            type="text"
+                            id="notesInput"
+                            name="notes"
+                            maxLength="35"
+                            size="5"
+                        />
+                    </div>
+                    <div>
+                        <input id="submit-btn" type="submit" />
+                    </div>
                 </div>
-                <div>
-                    <input id="submit-btn" type="submit" />
-                </div>
-            </div>
-        </form>
+            </form>
         </Wrapper>
-    )
-}
+    );
+};
 
 export default Attendance;
