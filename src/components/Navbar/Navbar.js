fae0527edb2e0f337f83d53629c3f85546292f17
Alexander Besse <alexanderjoeb@hotmail.com>
2020-04-02 13:04:53 -0500

    Burger menu added.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-
-const NavbarWrap = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  width: 100%;
-  height: 80px;
-  background-color: #1b212c;
-  color: white;
-  .menu {
-    margin: 0 10px;
-    @media screen and (min-width: 636px) {
-      display: none;
-    }
-  }
-  .title{
-    display: flex;
-    justify-content: center;
-  }
-`;
+import Burger from "./Burger/index";
 
 const Navbar = () => {
-  return (
-    <NavbarWrap>
-      <div className="menu">Menu</div>
-      <div className='title'>User Name</div>
-    </NavbarWrap>
-  );
+	const [open, setOpen] = useState(false);
+	return (
+		<>
+			<NavbarWrap>
+				<div className='menu'>
+					<Burger open={open} setOpen={setOpen} />
+				</div>
+				<div className='title'>User Name</div>
+      </NavbarWrap>
+      
+		</>
+	);
 };
+
+const NavbarWrap = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	width: 100%;
+	height: 80px;
+	background-color: #1b212c;
+	color: white;
+	@media screen and (min-width: 636px) {
+		display: none;
+	}
+	.menu {
+		margin: 0 10px;
+		order: 1;
+	}
+	.title {
+		display: flex;
+		justify-content: center;
+	}
+`;
 
 export default Navbar;
