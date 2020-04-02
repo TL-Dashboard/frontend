344ef3e9b8993b736164b92fa1129a9e05ad0cd5
Robert Gordon <Komeuppance@gmail.com>
2020-04-02 09:37:42 -0700

    Adjust code for React

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,32 +3,27 @@ import {ReactComponent as Logo} from "../../assets/LambdaLogo.svg";
 
 
 const Login = () => {
-    return(
     
+    return(
         <div className="login">
             <form className="login__container">
             <Logo className="login__logo"/>
                 <div className="login__container__items">
                     <label>Email Address</label><br/>
-                    <input className="login__container__items__input" type="email">
-                    </input>
+                    <input className="login__container__items__input" type="email" />
                 </div>
                 <div className="login__container__items">
                     <label>Password</label><br/>
-                    <input className="login__container__items__input" type="password">
-                    </input>
+                    <input className="login__container__items__input" type="password" />
                 </div>
-
                 <div className="login__container__items">
                         <input className="login__container__items__input__submitButn" type="submit" />
                 </div>            
-                  
                 <div className="login__container__forgotText">
-                    <p>Forgot <a className="login__container__forgotText__forgotLinks" href="#">Username/Password?</a></p>
+                    <p>Forgot <a className="login__container__forgotText__forgotLinks" href="/">Username/Password?</a></p>
                 </div>
             </form>
         </div>
-
     )
 }
 
