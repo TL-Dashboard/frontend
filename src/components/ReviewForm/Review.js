cc1f3257ed9100c6fd848cd7b8284b3a07e443e1
Angelg10 <angelguzman97@gmail.com>
2020-04-03 15:20:14 -0400

    fixed smile emoji typo

--- src/components/ReviewForm/Review.js
+++ src/components/ReviewForm/Review.js
@@ -60,7 +60,7 @@ return (
     <div className="review__container__smallcontainer">
     <label htmlFor="review__container__smallcontainer--item">Grade </label>
         <select className="review__container__smallcontainer--item" name="date">
-            <option value='great'>😀; I'm feeling great</option>
+            <option value='great'>😀 I'm feeling great</option>
             <option value='okay'>😐 I'm feeling okay</option>
             <option value="hard">🙁 Okay, this is hard</option>
             <option value='discouraged'>😡 I'm feeling discouraged</option>
