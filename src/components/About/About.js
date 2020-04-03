b023c929bae1e9ef7ea9c8a9a958548412b26533
Angelg10 <angelguzman97@gmail.com>
2020-04-03 18:17:34 -0400

    changed about page layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,24 +5,24 @@ const About = () => {
             <div className="about">
                 <div className="about__container">
                     <div className="about__container__smallcontainer">
-                        <p>Robert Gordon</p>
-                        <img src="https://ca.slack-edge.com/T4JUEB3ME-UN66JA72N-811a63af5d29-512" alt="picture of Robert Gordon" height="200px" />
+                        <p><strong>Robert Gordon</strong></p>
+                        <img src="https://ca.slack-edge.com/T4JUEB3ME-UN66JA72N-811a63af5d29-512" alt="Robert Gordon" height="200px" />
                         <p>Project Lead / Dev</p>
                     </div>
                 </div>
            
                 <div className="about__container">
                     <div className="about__container__smallcontainer">
-                        <p>Alexander Besse</p>
-                        <img src="https://ca.slack-edge.com/T4JUEB3ME-URLATSSKA-308016b79b83-512" alt="picture of Alexander Besse" height="200px" />
+                        <p><strong>Alexander Besse</strong></p>
+                        <img src="https://ca.slack-edge.com/T4JUEB3ME-URLATSSKA-308016b79b83-512" alt="Alexander Besse" height="200px" />
                         <p>UX / React Dev</p>
                     </div>
                 </div>
 
                 <div className="about__container">
                     <div className="about__container__smallcontainer">
-                        <p>Angel Guzman</p>
-                        <img src="https://ca.slack-edge.com/T4JUEB3ME-UTGCH2FQV-d0c2a3fd1ecb-512"  alt="picture of Angel Guzman" height="200px" />
+                        <p><strong>Angel Guzman</strong></p>
+                        <img src="https://ca.slack-edge.com/T4JUEB3ME-UTGCH2FQV-d0c2a3fd1ecb-512"  alt="Angel Guzman" height="200px" />
                         <p>UX / React Dev</p>
                     </div>
                 </div> 
