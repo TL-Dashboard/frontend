0c75787a563e2d84537823750aa249b1be520274
Robert Gordon <Komeuppance@gmail.com>
2020-04-03 03:50:34 -0700

    Adjust styles

--- src/components/Tiles/MissingWork.js
+++ src/components/Tiles/MissingWork.js
@@ -7,11 +7,24 @@ const MissingWork = ({context}) => {
 
     let missing = []
 
+    function compare(a, b) {
+        const itemA = a.assignment.id;
+        const itemB = b.assignment.id;
+      
+        let comparison = 0;
+        if (itemA > itemB) {
+          comparison = 1;
+        } else if (itemA < itemB) {
+          comparison = -1;
+        }
+        return comparison * -1;
+    }
+
     const checkIfRetroExistsForAssignment = () => {
         if (students.length > 0) {
             students.map(student => {
                 if (student.retros.length < 5){
-                    for (let i = 4; i >= 0; i--){
+                    for (let i = 5; i >= 0; i--){
                         if (student.retros[i]?.assignment_id === undefined){
                             // console.log(student.first_name, 'missing!', assignments[i].id)
                             missing = [ ...missing,
@@ -24,6 +37,8 @@ const MissingWork = ({context}) => {
                         }
                     }
                 }
+                missing.sort(compare)
+                // console.log(missing)
                 return missing
             })
         }
@@ -34,7 +49,6 @@ const MissingWork = ({context}) => {
         <div className="tile__missing__headers">
             <p>Student</p>
             <p>Assignment</p>
-            
         </div>
         {checkIfRetroExistsForAssignment()}
        { 
@@ -44,9 +58,10 @@ const MissingWork = ({context}) => {
                 <div className="tile__missing__container" key={index}>
                     <div className="tile__missing__container__student">
                         <p>{item.student.first_name}</p>
+                        <p>{item.student.last_name}</p>
                     </div>
                     <a href={item.assignment.url} target="_blank" rel="noopener noreferrer">
-                        <div className="tile__missing__container__student__assignment">
+                        <div className={`tile__missing__container__student__${item.assignment.type}`}>
                             <p>{item.assignment.name}</p>
                         </div>
                     </a>
